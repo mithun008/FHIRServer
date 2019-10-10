422d687f5a67d948766a90d012142e33c0810f9b
Mithun Mallick <mithumal@amazon.com>
2019-10-10 16:38:19 -0700

    fixed to have all the exports included

--- resources/setupEnv.sh
+++ resources/setupEnv.sh
@@ -1,3 +1,13 @@
+wget http://apache.mirrors.pair.com/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz 
+tar xvf apache-maven-3.6.1-bin.tar.gz
+sudo mv apache-maven-3.6.1 /usr/local/apache-maven
+
+export M2_HOME=/usr/local/apache-maven
+export M2=$M2_HOME/bin
+export PATH=$M2:$PATH
+source ~/.bashrc
+
+
 #install jdk 1.8 
 sudo yum install java-1.8.0-openjdk-devel
 
@@ -5,10 +15,5 @@ sudo yum install java-1.8.0-openjdk-devel
 sudo alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java
 
 sudo yum remove java-1.7.0-openjdk-devel
-
-#install maven
-sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven
 
 sudo yum install jq
